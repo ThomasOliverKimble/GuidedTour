2706c3095c658801610a88bbb7b9c45a341e3fb6
ThomasOliverKimble <tokimble@gmail.com>
2022-04-06 14:07:26 +0200

    removed files

--- spot/controllers/spot_moving_demo/build/release/spot_moving_demo.d
+++
@@ -1,9 +0,0 @@
-build/release/spot_moving_demo.o: spot_moving_demo.c \
- /home/thomas/webots/include/controller/c/webots/camera.h \
- /home/thomas/webots/include/controller/c/webots/camera_recognition_object.h \
- /home/thomas/webots/include/controller/c/webots/types.h \
- /home/thomas/webots/include/controller/c/webots/device.h \
- /home/thomas/webots/include/controller/c/webots/nodes.h \
- /home/thomas/webots/include/controller/c/webots/led.h \
- /home/thomas/webots/include/controller/c/webots/motor.h \
- /home/thomas/webots/include/controller/c/webots/robot.h
