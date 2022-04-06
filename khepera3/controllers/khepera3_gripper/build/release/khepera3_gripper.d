27c079f80d9e386ec1c6b928b97e6c268134ce2b
ThomasOliverKimble <tokimble@gmail.com>
2022-04-06 10:49:41 +0200

    Khepera3 robot added

---
+++ khepera3/controllers/khepera3_gripper/build/release/khepera3_gripper.d
@@ -0,0 +1,6 @@
+build/release/khepera3_gripper.o: khepera3_gripper.c \
+ /home/thomas/webots/include/controller/c/webots/distance_sensor.h \
+ /home/thomas/webots/include/controller/c/webots/types.h \
+ /home/thomas/webots/include/controller/c/webots/motor.h \
+ /home/thomas/webots/include/controller/c/webots/robot.h \
+ /home/thomas/webots/include/controller/c/webots/nodes.h
